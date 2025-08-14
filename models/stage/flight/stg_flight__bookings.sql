4f0c6a1b8419c6662030538210dd9e3e646c7484
Alex Ugarov <alexuu@mail.ru>
2025-08-14 17:04:37 +0600

    add incremental load

--- models/stage/flight/stg_flight__bookings.sql
+++ models/stage/flight/stg_flight__bookings.sql
@@ -1,14 +1,16 @@
 {{ 
     config(
-        materialized = 'ephemeral',
-        on_configuration_change = "apply",
-        indexes = [
-            {
-                "columns": ["book_date"],
-                "unique": false,
-            }
-        ]
+        materialized = 'incremental',
+        incremental_strategy  "merge",
+        tags = ['bookings'],
+        merge_update_columns = ['total_amount'],
+        merge_exclude_columns = ['book_date'],
+        incremental_predicates = ["DBT_INTERNAL_DEST.book_date > current_timestamp - '7 day'::interval"] 
     )
 }}
 select book_ref, book_date, total_amount
 from {{ source('demo_src', 'bookings')}}
+{% if is_incremental() %}
+WHERE
+    book_ref > (SELECT max(book_ref) FROM {{ source('demo_src', 'bookings')}}) - '7 day'::interval
+{% endif %}
