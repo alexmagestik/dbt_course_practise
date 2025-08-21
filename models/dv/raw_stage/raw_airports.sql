8e8fd2473fd69312c94b4b443914d1c86617a36c
Alex Ugarov <alexuu@mail.ru>
2025-08-21 17:19:38 +0600

    add automatic Data Vault generation

---
+++ models/dv/raw_stage/raw_airports.sql
@@ -0,0 +1,16 @@
+{{
+    config(
+        schema = "dv",
+        materialized = "table"
+    )
+}}
+SELECT
+    airport_code,
+    airport_name,
+    city,
+    coordinates,
+    timezone,
+    'bookings' as RECORD_SOURCE,
+    now() as LOAD_DATE
+FROM
+    {{ source('demo_src', 'airports') }}
